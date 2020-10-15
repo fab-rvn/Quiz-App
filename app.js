09a30559cf2f7b7a00095b5218585549e9d988d7
faber <faber@faber.local>
2020-10-15 14:14:25 +0200

    Update progress bar, rules and sort high scores

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ const enterButton = document.getElementById('enter-button');
 const homeSection = document.getElementById('home');
 const welcomeTextElement = document.getElementById('welcome-text');
 const gameButton = document.getElementById('start-game-button');
+const rules = document.getElementById('rules');
+const rulesButton = document.getElementById('rules-button');
+const iconCloseRules = document.getElementById('close-icon');
 const gameSection = document.getElementById('game');
+const progressBar = document.getElementById('progress-bar');
+const topic = Array.from(document.getElementsByClassName('step'));
 const questionTextElement = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const gameOver = document.getElementById('game-over');
 const finalTextElement = document.getElementById('final-text');
 const scoreTextElement = document.getElementById('final-score');
 const highScoresList = document.getElementById('high-scores-list');
+const sortUpIcon = document.getElementById('sort-up-icon');
+const sortDownIcon = document.getElementById('sort-down-icon');
 const playAgainButton = document.getElementById('play-again-button');
 const goHomeButton = document.getElementById('go-home-button');
 
@@ -25,7 +32,7 @@ let questionCounter = 0;
 let acpetingAnswer = false;
 
 const MAX_BONUS = 5;
-const MAX_QUESTIONS = 4;
+const MAX_QUESTIONS = 20;
 
 enterButton.addEventListener('click', e => {
   e.preventDefault();
@@ -39,6 +46,26 @@ gameButton.addEventListener('click', () => {
   gameSection.classList.toggle('hide');
   homeSection.classList.toggle('hide');
   startGame();
+})
+
+rulesButton.addEventListener('click', () => {
+  rules.classList.toggle('hide');
+})
+
+iconCloseRules.addEventListener('click', () => {
+  rules.classList.toggle('hide');
+})
+
+sortUpIcon.addEventListener("click", () => {
+  highScoresList.innerHTML = "";
+  users.sort((a,b) => b.score - a.score)
+  .map(user => highScoresList.innerHTML += `<li class="score-list-item">${user.name} - ${user.score}</li>`);
+})
+
+sortDownIcon.addEventListener('click', () => {
+  highScoresList.innerHTML = "";
+  users.sort((a,b) => a.score - b.score)
+  .map(user => highScoresList.innerHTML += `<li class="score-list-item">${user.name} - ${user.score}</li>`);
 })
 
 playAgainButton.addEventListener('click', () => {
@@ -74,23 +101,30 @@ function startGame() {
 
 function nextQuestion() {
   if (questionCounter >= MAX_QUESTIONS || availableQuestions.length === 0) {
+    // SAVE THE SCORE AND STYLE IT
     user.score = score;
-
     if (score >= 60) {
-      finalTextElement.innerText = `Well done ${user.name}`;
+      finalTextElement.innerText = `Well done ${user.name}!`;
       scoreTextElement.style.color = 'var(--correct)';
     } else {
       finalTextElement.innerText = `${user.name} You can do better than this..`;
       scoreTextElement.style.color = 'var(--wrong)';
     }
-
     scoreTextElement.innerText = score;
-    highScoresList.innerHTML = users.map(user => `<li class="score-list-item">${user.name} - ${user.score}</li>`).join('');
+    // RENDER ALL THE USER IN MEMORY
+    highScoresList.innerHTML += `<li class="score-list-item">${user.name} - ${user.score}</li>`;
     gameOver.classList.toggle('hide');
     gameSection.classList.toggle('hide');
   }
   
   questionCounter++;
+  // UPDATE PROGRESS BAR
+  progressBar.style.width = (questionCounter / MAX_QUESTIONS) * 100 + '%';
+  if (questionCounter > 12) {
+    topic[2].classList.add('active');
+  } else if (questionCounter > 6) {
+    topic[1].classList.add('active');
+  }
   // DISPLAY QUESTIONS
   const questionIndex = availableQuestions.length -1;
   currentQuestion = availableQuestions[questionIndex];
